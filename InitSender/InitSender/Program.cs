c1ffbc092ba761f5aad058ec9ad0348130329e9e
kuuube <hexagonisalie@gmail.com>
2022-05-02 15:22:59 -0400

    Feat: Added command line args support

--- InitSender/InitSender/Program.cs
+++ InitSender/InitSender/Program.cs
@@ -11,36 +11,68 @@ namespace InitSender
         {
             string tablet_model;
             string init_file;
-            int tablet_pid;
             int tablet_vid = 1386;
+            int tablet_pid = 0;
+            int init_delay = 0;
 
-            Console.WriteLine("Send inits to which tablet?\n0. Custom\n1. PTK-540WL\n2. PTK-640\n3. PTK-840\n4. PTK-1240");
-            tablet_model = Console.ReadLine();
-
-            switch (tablet_model)
+            if (args.Length != 0)
             {
-                case "1": //PTK-540WL
-                    tablet_pid = 188;
-                    break;
-                case "2": //PTK-640
-                   tablet_pid = 185;
-                    break;
-                case "3": //PTK-840
-                    tablet_pid = 186;
-                    break;
-                case "4": //PTK-1240
-                   tablet_pid = 187;
-                    break;
-                default:
-                    Console.WriteLine("Set custom VID and PID (base10)\nVID:");
-                    tablet_vid = Int16.Parse(Console.ReadLine());
-                    Console.WriteLine("PID:");
-                    tablet_pid = Int16.Parse(Console.ReadLine());
-                    break;
+                tablet_model = args[0];
+                init_file = args[1];
+                init_delay = Int16.Parse(args[2]);
+                switch (tablet_model)
+                {
+                    case "1": //PTK-540WL
+                        tablet_pid = 188;
+                        break;
+                    case "2": //PTK-640
+                        tablet_pid = 185;
+                        break;
+                    case "3": //PTK-840
+                        tablet_pid = 186;
+                        break;
+                    case "4": //PTK-1240
+                        tablet_pid = 187;
+                        break;
+                    default:
+                        tablet_vid = Int16.Parse(args[3]);
+                        tablet_pid = Int16.Parse(args[4]);
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Send inits to which tablet?\n0. Custom\n1. PTK-540WL\n2. PTK-640\n3. PTK-840\n4. PTK-1240");
+                tablet_model = Console.ReadLine();
+
+                switch (tablet_model)
+                {
+                    case "1": //PTK-540WL
+                        tablet_pid = 188;
+                        break;
+                    case "2": //PTK-640
+                        tablet_pid = 185;
+                        break;
+                    case "3": //PTK-840
+                        tablet_pid = 186;
+                        break;
+                    case "4": //PTK-1240
+                        tablet_pid = 187;
+                        break;
+                    default:
+                        Console.WriteLine("Set custom VID and PID (base10)\nVID:");
+                        tablet_vid = Int16.Parse(Console.ReadLine());
+                        Console.WriteLine("PID:");
+                        tablet_pid = Int16.Parse(Console.ReadLine());
+                        break;
+                }
+
+                Console.WriteLine("File to use for inits");
+                init_file = Console.ReadLine();
+                Console.WriteLine("Init delay");
+                init_delay = Int16.Parse(Console.ReadLine());
             }
 
-            Console.WriteLine("File to use for inits");
-            init_file = Console.ReadLine();
             string[] features = File.ReadLines(init_file).ToArray();
             for (int i = 0; i < features.Length; i++)
             {
@@ -57,7 +89,7 @@ namespace InitSender
 
             if (tablet == null)
             {
-                Console.WriteLine("Failed to find PTK-1240");
+                Console.WriteLine("Failed to find tablet\nPID:" + tablet_pid + " VID:" + tablet_vid);
                 return;
             }
 
@@ -72,6 +104,7 @@ namespace InitSender
                         i = 0;
                     var feature = Convert.FromBase64String(features[i]);
                     hidStream.SetFeature(feature);
+                    System.Threading.Thread.Sleep(init_delay);
                     i++;
                 }
             }
